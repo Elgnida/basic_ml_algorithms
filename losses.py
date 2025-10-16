432fd9b6ad93d829db6f2feea47395860ee4363f
Elgnida <vaguenessofmind@gmail.com>
2025-10-16 20:08:21 +0300

    Added new function

--- losses.py
+++ losses.py
@@ -8,3 +8,21 @@ class L2_loss:
         w_grad = (2 / X.shape[0]) * (X.T @ (np.dot(X, w) + b - y) + 2 * C * w)
         b_grad = (2 / X.shape[0]) * np.sum((X @ w) + b - y)
         return w_grad, b_grad
+
+class L1_loss:
+    def __call__(self, X, y, w, b, C):
+        return np.sum((X @ w + b - y) ** 2) + C * np.sum(np.abs(w))
+
+    def grad(self, X, y, w, b, C):
+        w_grad = (2 / X.shape[0]) * (X.T @ (np.dot(X, w) + b - y)) + C * np.sign(w)
+        b_grad = (2 / X.shape[0]) * np.sum((X @ w) + b - y)
+        return w_grad, b_grad
+
+class MSE_loss:
+    def __call__(self, X, y, w, b, C):
+        return np.sum((X @ w + b - y) ** 2)
+
+    def grad(self, X, y, w, b, C):
+        w_grad = (2 / X.shape[0]) * (X.T @ (np.dot(X, w) + b - y))
+        b_grad = (2 / X.shape[0]) * np.sum((X @ w) + b - y)
+        return w_grad, b_grad
