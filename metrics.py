c6288b40a44aa3c4aaac9422add48c2e63678396
Elgnida <vaguenessofmind@gmail.com>
2025-10-16 20:08:33 +0300

    Added new metrics

--- metrics.py
+++ metrics.py
@@ -1,4 +1,7 @@
 import numpy as np
 
 def mse(y_pred, y_true):
-    return np.sum((y_pred - y_true) ** 2) / y_true.size
+    return np.sum((y_pred - y_true) ** 2).mean()
+
+def mae(y_pred, y_true):
+    return np.sum(np.abs(y_pred - y_true)).mean()
